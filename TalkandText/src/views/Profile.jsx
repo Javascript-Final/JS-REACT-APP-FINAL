3e1136a45306b85bd87fd7e61fcf16762b6091b8
Desislava Velinova <desislavatsvelinova@gmail.com>
2024-02-28 23:08:13 +0200

    add edit profile

--- TalkandText/src/views/Profile.jsx
+++ TalkandText/src/views/Profile.jsx
@@ -1,24 +1,75 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AppContext } from "../context/AppContext";
+import { updateUser } from "../services/user-service";
 
 export const Profile = () => {
-    const { userData, setContext } = useContext(AppContext);
+    const { user, userData, setContext } = useContext(AppContext);
+    const [isEditing, setIsEditing] = useState(false);
+    const [form, setForm] = useState({
+        firstName: userData?.firstName,
+        lastName: userData?.lastName,
+        phoneNumber: userData?.phoneNumber,
+    });
 
-    return (
+    const updateForm = (prop) => (e) => {
+        setForm({ ...form, [prop]: e.target.value });
+    };
+
+    const submit = () => {
+        const updatedUserData = {...userData, ...form}
+        updateUser(userData.username, updatedUserData);
+        setContext({user, userData: updatedUserData})
+        setIsEditing(false);
+    }
+
+    return !isEditing ?
         <>
             <h1>Profile</h1>
             <div>
-                first name: {userData.firstName}
+                first name: {userData?.firstName}
             </div>
             <div>
-                last name: {userData.lastName}
+                last name: {userData?.lastName}
             </div>
             <div>
-                username: {userData.username}
+                username: {userData?.username}
             </div>
             <div>
-                phone number:  {userData.phoneNumber}
+                phone number: {userData?.phoneNumber}
             </div>
+            <button onClick={() => setIsEditing(true)}>Edit Profile</button>
         </>
-    )
+        :
+        <>
+            <form>
+                <label htmlFor="firstName">first name</label>
+                <input
+                    value={form.firstName}
+                    onChange={updateForm("firstName")}
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                />
+                <br />
+                <label htmlFor="lastName">last name</label>
+                <input
+                    value={form.lastName}
+                    onChange={updateForm("lastName")}
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                />
+                <br />
+                <label htmlFor="phoneNumber">phone number</label>
+                <input
+                    value={form.phoneNumber}
+                    onChange={updateForm("phoneNumber")}
+                    type="tel"
+                    name="phoneNumber"
+                    id="phoneNumber"
+                />
+                <br />
+            </form>
+            <button onClick={submit}>Save</button>
+        </>
 }
