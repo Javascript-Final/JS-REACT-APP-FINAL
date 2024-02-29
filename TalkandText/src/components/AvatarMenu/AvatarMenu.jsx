d33b7e818ca944cb77258488f842581895bf1a36
Desislava Velinova <desislavatsvelinova@gmail.com>
2024-02-29 13:04:39 +0200

    add drop down menu to profile bar in header

---
+++ TalkandText/src/components/AvatarMenu/AvatarMenu.jsx
@@ -0,0 +1,86 @@
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import React from 'react';
+import { Avatar, Divider, ListItemIcon } from '@mui/material';
+import { Settings, PersonAdd, Logout } from '@mui/icons-material';
+import { NavLink, useNavigate } from "react-router-dom"
+import { logout } from '../../services/auth-service';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+
+export const AvatarMenu = ({ open, anchorEl, handleClose }) => {
+
+    const navigate = useNavigate();
+
+    const { user, userData, setContext } = useContext(AppContext);
+
+    const logoutUser = async () => {
+        await logout();
+        navigate('/')
+        setContext({ user: null, userData: null })
+    }
+
+    return (
+        <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+                elevation: 0,
+                sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                    },
+                    '&::before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                    },
+                },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+            <MenuItem onClick={() => { navigate('/profile') }}>
+                <Avatar /> Profile
+            </MenuItem>
+            <MenuItem onClick={() => { navigate('/create-channel') }}>
+                <Avatar /> Create channel
+            </MenuItem>
+            <Divider />
+            {/* <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                    <PersonAdd fontSize="small" />
+                </ListItemIcon>
+                Add another account
+            </MenuItem> */}
+            {/* <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                    <Settings fontSize="small" />
+                </ListItemIcon>
+                Settings
+            </MenuItem> */}
+            <MenuItem onClick={logoutUser}>
+                <ListItemIcon>
+                    <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+            </MenuItem>
+        </Menu>
+    )
+}
--- TalkandText/src/components/Header/Header.jsx
+++ TalkandText/src/components/Header/Header.jsx
@@ -1,45 +1,65 @@
 import React from 'react'
 import { NavLink, useNavigate } from "react-router-dom"
-import { logout } from '../../services/auth-service'; 
+import { logout } from '../../services/auth-service';
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from '../../context/AppContext'
+import Avatar from '@mui/material/Avatar';
+import { StyledBadge } from '../AvatarMenu/StyledBadge';
+import { AvatarMenu } from '../AvatarMenu/AvatarMenu';
 
 export function Header() {
 
-    const { user, userData, setContext } = useContext(AppContext);
-    
+  const { user, userData, setContext } = useContext(AppContext);
 
-    const [avatarUrl, setAvatarUrl] = useState(userData?.avatarUrl);
+  const [avatarUrl, setAvatarUrl] = useState(userData?.avatarUrl);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      setAvatarUrl(userData?.avatarUrl);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    setAvatarUrl(userData?.avatarUrl);
   }, [userData]);
-    // console.log(userData);
 
-    const logoutUser = async () => {
-      await logout();
-      setContext({ user: null, userData: null })
-  }
+  // const logoutUser = async () => {
+  //   await logout();
+  //   setContext({ user: null, userData: null })
+  // }
 
   return (
     <header>
-        <NavLink to='/'>Home</NavLink>
-        {user 
+      <NavLink to='/'>Home</NavLink>
+      {user
         ? (
-            <>
+          <>
             {`Welcome, ${userData?.username}`}
-            <NavLink to='/create-channel'>Create Channel</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <button onClick={logoutUser}>Logout</button>
-            </>
+            {/* <NavLink to='/create-channel'>Create Channel</NavLink> */}
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              onClick={handleClick}
+              variant="dot"
+            >
+              <Avatar alt="User Avatar" src={userData?.avatarUrl} />
+            </StyledBadge>
+            <AvatarMenu open={open} anchorEl={anchorEl} handleClose={handleClose} />
+            {/* <NavLink to="/profile">Profile</NavLink> */}
+            {/* <button onClick={logoutUser}>Logout</button> */}
+          </>
         )
         : (
-            <>
+          <>
             <NavLink to='/register'>Register</NavLink>
             <NavLink to='/login'>Login</NavLink>
-            </>
+          </>
         )}
     </header>
   )
