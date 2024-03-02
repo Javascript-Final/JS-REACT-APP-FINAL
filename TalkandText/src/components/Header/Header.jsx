18148a3b553781c93a3157e8ca012bf5cbcaf995
Desislava Velinova <desislavatsvelinova@gmail.com>
2024-03-02 17:34:27 +0200

    removed unused imports from header component

--- TalkandText/src/components/Header/Header.jsx
+++ TalkandText/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink, useNavigate } from "react-router-dom"
-import { logout } from '../../services/auth-service';
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from '../../context/AppContext'
 import Avatar from '@mui/material/Avatar';
@@ -10,18 +9,9 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
 
 export function Header() {
 
