7ba1007ce967b0d85fcb5db809f11341c6890db9
Desislava Velinova <desislavatsvelinova@gmail.com>
2024-03-06 22:43:09 +0200

    fixed a bit of view to the single user prfofile

--- TalkandText/src/views/SingleUserProfileView.jsx
+++ TalkandText/src/views/SingleUserProfileView.jsx
@@ -34,67 +34,132 @@ export const SingleUserProfileView = () => {
     //     }
     // }, [user])
 
+    //     return (
+    //         <Container sx={{ mt: 6 }}>
+    //             <Grid container spacing={6}>
+    //                 <Grid item xs={3}>
+    //                     <Avatar p={2} sx={{ height: '220px', width: '220px', background: "rgb(240, 240, 240)" }} alt="User Avatar" src={userData?.avatarUrl} />
+    //                 </Grid>
+    //                 <Grid item xs={3} p={2}>
+    //                     <TextField
+    //                         margin="normal"
+    //                         required
+    //                         id="handle"
+    //                         label="Handle"
+    //                         name="handle"
+    //                         disabled
+    //                         value={userData?.username}
+    //                         fullWidth
+    //                         InputLabelProps={{ shrink: true }}
+    //                     />
+    //                 </Grid>
+    //                 <Grid item xs={9}>
+    //                     <Paper>
+    //                         <Grid container p={3}>
+    //                             <Grid item xs={4} p={2}>
+    //                                 <TextField
+    //                                     margin="normal"
+    //                                     required
+    //                                     id="firstName"
+    //                                     label="First Name"
+    //                                     disabled
+    //                                     value={userData?.firstName}
+    //                                     fullWidth
+    //                                     InputLabelProps={{ shrink: true }}
+    //                                 />
+    //                             </Grid>
+    //                             <Grid item xs={4} p={2}>
+    //                                 <TextField
+    //                                     margin="normal"
+    //                                     required
+    //                                     id="lastName"
+    //                                     label="Last Name"
+    //                                     name="lastName"
+    //                                     disabled
+    //                                     value={userData?.lastName}
+    //                                     fullWidth
+    //                                     InputLabelProps={{ shrink: true }}
+    //                                 />
+    //                             </Grid>
+    //                             <Grid item xs={4} p={2}>
+    //                                 <Stack direction="row" spacing={2}>
+    //                                     <Button variant="contained" color="success">
+    //                                         Send a DM
+    //                                     </Button>
+    //                                     <Button variant="contained" color="primary">
+    //                                         Add to team
+    //                                     </Button>
+    //                                 </Stack>
+    //                             </Grid>
+    //                         </Grid>
+    //                     </Paper>
+    //                 </Grid>
+    //             </Grid>
+    //         </Container>
+    //     )
+    // }
+
+
     return (
-        <Container sx={{ mt: 6 }}>
-            <Grid container spacing={6}>
-                <Grid item xs={3}>
-                    <Avatar p={2} sx={{ height: '220px', width: '220px', background: "rgb(240, 240, 240)" }} alt="User Avatar" src={userData?.avatarUrl} />
-                </Grid>
-                <Grid item xs={3} p={2}>
-                    <TextField
-                        margin="normal"
-                        required
-                        id="handle"
-                        label="Handle"
-                        name="handle"
-                        disabled
-                        value={userData?.username}
-                        fullWidth
-                        InputLabelProps={{ shrink: true }}
-                    />
-                </Grid>
-                <Grid item xs={9}>
-                    <Paper>
-                        <Grid container p={3}>
-                            <Grid item xs={4} p={2}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    id="firstName"
-                                    label="First Name"
-                                    disabled
-                                    value={userData?.firstName}
-                                    fullWidth
-                                    InputLabelProps={{ shrink: true }}
-                                />
-                            </Grid>
-                            <Grid item xs={4} p={2}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    disabled
-                                    value={userData?.lastName}
-                                    fullWidth
-                                    InputLabelProps={{ shrink: true }}
-                                />
-                            </Grid>
-                            <Grid item xs={4} p={2}>
-                                <Stack direction="row" spacing={2}>
-                                    <Button variant="contained" color="success">
-                                        Send a DM
-                                    </Button>
-                                    <Button variant="contained" color="primary">
-                                        Add to team
-                                    </Button>
-                                </Stack>
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Container>
-    )
+<Container sx={{ mt: 6 }}>
+  <Grid container spacing={6}>
+    <Grid item xs={3}>
+      <Avatar p={2} sx={{ height: '220px', width: '220px', background: "rgb(240, 240, 240)" }} alt="User Avatar" src={userData?.avatarUrl} />
+      <TextField
+        margin="normal"
+        required
+        id="handle"
+        label="Handle"
+        name="handle"
+        disabled
+        value={userData?.username}
+        fullWidth
+        InputLabelProps={{ shrink: true }}
+      />
+    </Grid>
+    <Grid item xs={9}>
+      <Paper>
+        <Grid container p={3}>
+          <Grid item xs={6} p={4}>
+            <TextField
+              margin="normal"
+              required
+              id="firstName"
+              label="First Name"
+              disabled
+              value={userData?.firstName}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={6} p={4}>
+            <TextField
+              margin="normal"
+              required
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              disabled
+              value={userData?.lastName}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={6} p={4}>
+            <Button variant="contained" color="success" fullWidth>
+              Send a DM
+            </Button>
+          </Grid>
+          <Grid item xs={6} p={4}>
+            <Button variant="contained" color="primary" fullWidth>
+              Add to team
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  </Grid>
+</Container>
+
+)
 }
